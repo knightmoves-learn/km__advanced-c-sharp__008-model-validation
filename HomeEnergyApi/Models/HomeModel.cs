54e63496a574f2d1797104a7cfcd918a7906d450
Isaac <isaac@knightmoves.org>
2025-01-17 16:37:37 -0600

    QA

--- HomeEnergyApi/Models/HomeModel.cs
+++ HomeEnergyApi/Models/HomeModel.cs
@@ -1,1 +1,24 @@
-public record Home(int Id, string OwnerLastName, string StreetAddress, string City, int MonthlyElectricUsage);
+using System.ComponentModel.DataAnnotations;
+
+public class Home
+{
+    [Required]
+    public int Id { get; init;}
+
+    [Required]
+    public string OwnerLastName { get; init;}
+    
+    [StringLength(40)]
+    public string? StreetAddress { get; init;}
+
+    public string? City { get; init;}
+
+    [Range(0, 40)]
+    public int? MonthlyElectricUsage { get; init;}
+}
+
+
+
+
+
+//(int Id, string OwnerLastName, string StreetAddress, string City, int MonthlyElectricUsage);
